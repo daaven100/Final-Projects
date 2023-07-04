275f7ea3f0b99d42be9b0ce36250d3e8bc542d67
Daniel Avendano <128726847+daaven100@users.noreply.github.com>
2023-07-03 23:26:24 -0500

    ValuesToADatabase.js
    This code was used in my Digital Pressure Recorder Project. The code grabs all values coming in from a PLC, sorts them, and send them to a Database

--- ValuesToADatabase.js
+++ ValuesToADatabase.js
@@ -1,3 +1,4 @@
+// This code was used in my Digital Pressure Recorder Project. The code grabs all values coming in from a PLC, sorts them, and send them to a Database
 var query1 = "INSERT INTO pressure_data (date_collected, time_collected, tech_name, pressure_value, job_num, serial_num, notes) VALUES (" + msg.date + ", " + msg.time + ", “ + msg.customername + ”, “ + msg.analog + ", " + msg.jobnumber + ", " + msg.serialnumber + ”, ” + msg.description + “)";
 msg.topic = "sql";
 msg.payload = query1;
