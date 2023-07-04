f6068f8616944e360d5e55dfa52c75b5d0b0ab3b
Daniel Avendano <128726847+daaven100@users.noreply.github.com>
2023-07-03 23:25:04 -0500

    ListValues.js
    This code was used in my Digital Pressure Recorder project. The code grabs the listed values from a PLC and list them out using Node-RED software

--- ListValues.js
+++ ListValues.js
@@ -1,3 +1,4 @@
+// This code was used in my Digital Pressure Recorder project. The code grabs the listed values from a PLC and list them out using Node-RED software
 msg.payload = {
         job_number: msg.jobnumber,
         serial_number: msg.serialnumber,
